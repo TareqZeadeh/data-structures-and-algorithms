968efe92fd04662f3b3b9dc7b558a291abfb707b
Tareq Zeadeh <tareq.ha130@yahoo.com>
2021-11-01 17:46:19 +0200

    challenge 27 done

--- java/Insertion-Sort/app/src/main/java/Insertion/Sort/App.java
+++ java/Insertion-Sort/app/src/main/java/Insertion/Sort/App.java
@@ -21,9 +21,52 @@ public class App {
       }
       return arr;
     }
+
+    public static void mergeSort (int[] arr){
+      int inputLength = arr.length;
+      if (inputLength<2){return;}
+      int midIndex = inputLength/2;
+      int[] left = new int[midIndex];
+      int[] right = new int[inputLength - midIndex];
+      for(int i=0; i<midIndex;i++){left[i] = arr[i];}
+      for (int i=midIndex;i<inputLength;i++){right[i-midIndex] = arr[i];}
+      mergeSort(left);
+      mergeSort(right);
+      merge(arr,left,right);
+    }
+    public static void merge (int[] arr,int[] left,int[] right){
+    int leftSize = left.length;
+    int rightSize = right.length;
+    int i = 0, j = 0, k = 0;
+    while (i<leftSize && j<rightSize) {
+      if (left[i] <= right[j]) {
+        arr[k] = left[i];
+        i++;
+      } else {
+        arr[k] = right[j];
+        j++;
+      }
+      k++;
+    }
+      while (i<leftSize){
+        arr[k] = left[i];
+        i++;k++;
+      }
+      while (j<rightSize){
+        arr[k] = right[j];
+        j++;k++;
+      }
+
+    }
     public static void main(String[] args) {
       int arr[] = {8,4,23,42,16,15};
-      System.out.println(Arrays.toString(sort(arr)));
-
+//      System.out.println(Arrays.toString(sort(arr)));
+       mergeSort(arr);
+      System.out.print("[");
+       for(int i=0; i<arr.length;i++){
+         if(i==0){System.out.print(arr[i]);}
+         else {System.out.print(", "+arr[i]);}
+       }
+      System.out.print("]");
     }
 }
